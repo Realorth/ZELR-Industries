5bc4b4ad184a24ca7bc7f501df143dc695fa8d9c
Aiwen <47836762+azhou12@users.noreply.github.com>
2021-07-04 15:03:22 +0200

    Update CoordinadorJuego.cpp

--- src/CoordinadorJuego.cpp
+++ src/CoordinadorJuego.cpp
@@ -11,14 +11,14 @@ CoordinadorJuego::~CoordinadorJuego()
 
 void CoordinadorJuego::teclaEspecial(unsigned char key)
 {
-	if (estado == JUEGO)
+	if (estado == JUEGO || estado == MAPA2 || estado == MAPA3)
 		mundo.teclaEspecial(key);
 
 }
 
 void CoordinadorJuego::teclaEspecialUp(unsigned char key)
 {
-	if (estado == JUEGO)
+	if (estado == JUEGO || estado == MAPA2 || estado == MAPA3)
 		mundo.teclaEspecialUp(key);
 }
 
@@ -32,12 +32,12 @@ void CoordinadorJuego::tecla(unsigned char key)
 		if (key == 's')
 			exit(0);
 	}
-	else if (estado == JUEGO) {
+	else if (estado == JUEGO || estado == MAPA2 || estado == MAPA3) {
 		mundo.tecla(key);
 	}
 	else if (estado == GAMEOVER) {
 		if (key == 'c') {
-			mundo.setcaida(false);
+			mundo.setcaida(false);//Para reiniciar el juego
 			mundo.setImpacto(false);//Para reiniciar el juego			
 			estado = INICIO;
 
@@ -51,19 +51,20 @@ void CoordinadorJuego::tecla(unsigned char key)
 
 void CoordinadorJuego::mueve()
 {
-	if (estado == JUEGO) {
+	if (estado == JUEGO || estado == MAPA2 || estado == MAPA3) {
 		mundo.mueve();
-		//si se ha acabado el mapa, estado=FIN
-		//si se ha caido el hombre, o se ha quedado sin vidas, estado=GAMEOVER
 	}
-		
+
 }
 
 void CoordinadorJuego::dibuja()
 {
-	static bool flagLecturaMonedas = true;
+	static bool flagLecturaMonedas = true;//Flag para la lectura de las monedas(leer una vez)
+	static bool flagGameOver = true;//Flag para el game over
+	static bool flagMapa = true;//Flag para cambio de mapa
 
 	if (estado == INICIO) {
+		flagGameOver = true;
 		gluLookAt(0, 7.5, 30,//posicion ojo
 			0.0, 7.65, 0.0,//hacia donde mira
 			0.0, 1.0, 0.0);//definimos hacia arriba eje y
@@ -74,11 +75,11 @@ void CoordinadorJuego::dibuja()
 
 		ETSIDI::setTextColor(1, 1, 1);
 		ETSIDI::setFont("fuentes/Bitwise.ttf", 12);
-		ETSIDI::printxy("PULSE LA TECLA -E- PARA EMPEZAR", -5, 7);
-		ETSIDI::printxy("PULSE LA TECLA -S- PARA SALIR", -5, 6);
+		ETSIDI::printxy("PULSE LA TECLA -E- PARA EMPEZAR A JUGAR CON PISTOLERO", -9, 7);
+		ETSIDI::printxy("PULSE LA TECLA -S- PARA SALIR", -5, 4);
 		ETSIDI::printxy("ZERL INDUSTRIES", 2, 1);
 		ETSIDI::printxy("Monedas Disponibles: ", -3, 14);
-		//Impresión de las monedas disponibles en pantalla
+		//ImpresiÃ³n de las monedas disponibles en pantalla
 		if (flagLecturaMonedas) {
 			LecturaMonedas::leer(a);
 			m = a.c_str();
@@ -88,23 +89,99 @@ void CoordinadorJuego::dibuja()
 		ETSIDI::setTextColor(1, 1, 1);
 		ETSIDI::setFont("fuentes/Bitwise.ttf", 12);
 		ETSIDI::printxy(m, 5, 14);
+
+		if (atoi(m) >= 500)
+			ETSIDI::printxy("PULSE LA TECLA -R- PARA EMPEZAR A JUGAR CON ARQUERO", -9, 6);
+
+		if (atoi(m) >= 1000)
+			ETSIDI::printxy("PULSE LA TECLA -T- PARA EMPEZAR A JUGAR CON MAGO", -9, 5);
+
+		ETSIDI::setTextColor(1, 0, 0);
+		if (atoi(m) < 500)
+			ETSIDI::printxy("JUGADOR ARQUERO NO DISPONIBLE>>DESBLOQUEO CON 500 MONEDAS", -10, 6);
+		if (atoi(m) < 1000)
+			ETSIDI::printxy("JUGADOR MAGO NO DISPONIBLE>>DESBLOQUEO CON 1000 MONEDAS", -10, 5);
 	}
-	else if (estado == JUEGO){
+	else if (estado == JUEGO) {
+		//mundo.setFin(false);
+		mundo.setMapa(0);
 		mundo.dibuja();
-		if (mundo.getImpacto() || mundo.getcaida()) 
-		estado = GAMEOVER;
-		
-}
+
+		if (mundo.getImpacto() || mundo.getcaida()) {
+			flagMapa = true;
+			estado = GAMEOVER;
+		}
+
+		if (mundo.getFin()) {
+			mundo.setFin(false);//Reinicia el Fin para que pueda usarse en el mapa2
+			estado = MAPA2;
+			flagMapa = true;
+		}
+	}
+	else if (estado == MAPA2) {
+
+		if (flagMapa == true) {
+			mundo.setFin(false);
+			mundo.setHombrePosicion(0, 0);
+			flagMapa = false;
+			mundo.setMapa(1);
+		}
+
+		mundo.dibuja();
+
+		if (mundo.getImpacto() || mundo.getcaida()) {
+			estado = GAMEOVER;
+			flagMapa = true;//Para que pueda cambiar el mapa en la siguiente partida
+		}
+
+		if (mundo.getFin()) {
+			mundo.setFin(false);//Reiniciar el Fin para que se pueda usar en el mapa3
+			estado = MAPA3;
+			flagMapa = true;//Para que pueda cambiar el mapa en la siguiente partida
+		}
+	}
+
+
+	else if (estado == MAPA3) {
+
+		//static bool flag = true;
+		if (flagMapa == true) {
+			mundo.setHombrePosicion(0, 0);
+			mundo.setFin(false);
+			flagMapa = false;
+		}
+		mundo.setMapa(2);
+		mundo.dibuja();
+
+		if (mundo.getImpacto() || mundo.getcaida()) {
+			flagMapa = true;
+			estado = GAMEOVER;
+		}
+		if (mundo.getFin()) {
+			mundo.setFin(false);
+			estado = FIN;
+			flagMapa = true;
+		}
+
+	}
+
 	else if (estado == GAMEOVER) {
 		static bool flag = true;
+		//bool flagSonido = true;
+
 		mundo.dibuja();
-		auto ojo = mundo.getOjo();
 		ETSIDI::setTextColor(1, 0, 0);
 		ETSIDI::setFont("fuentes/Bitwise.ttf", 16);
-		ETSIDI::printxy("GAMEOVER: Has perdido...", ojo.x-5, 10);
-		ETSIDI::printxy("PULSE LA TECLA -C- PARA CONTINUAR", ojo.x-5, 5);
-		//Almacenamiento de las monedas adquiridas en el ficehro .txt
-		if (flag) {
+		ETSIDI::printxy("GAMEOVER: Has perdido...", -5 + mundo.getHombrePosicion().x, 10);
+		ETSIDI::printxy("PULSE LA TECLA -C- PARA CONTINUAR", -5 + mundo.getHombrePosicion().x, 5);
+		/*if (flagSonido) {
+			ETSIDI::play("sonidos/GameOver.wav");
+			flagSonido = false;
+		}*/
+
+		//Almacenamiento de las monedas adquiridas en el fichero .txt
+		if (flagGameOver) {
+			ETSIDI::play("sonidos/GameOver.wav");//Solo suena una vez cuando se pierde
 			ofstream dato("monedas.txt");
 			int inicial = atoi(m);//Convierte const char* a int para realizar la suma
 			if (dato.is_open()) {
@@ -113,6 +190,7 @@ void CoordinadorJuego::dibuja()
 			}
 			else cout << "Unable to open file";
 			flag = false;
+			flagGameOver = false;
 		}
 		//Vuelve a establecer el flag a true para que
 		//cuando se vuelva a la pantalla de inicio aparezca las monedas totales
@@ -122,10 +200,10 @@ void CoordinadorJuego::dibuja()
 		static bool flag = true;
 		mundo.dibuja();
 		ETSIDI::setFont("fuentes/Bitwise.ttf", 16);
-		ETSIDI::printxy("ENHORABUENA:¡¡Has triunfado!!", -5, 10);
-		ETSIDI::printxy("PULSE LA TECLA -C- PARA CONTINUAR", -5, 9);
+		ETSIDI::printxy("ENHORABUENA:Â¡Â¡Has triunfado!!", -5 + mundo.getHombrePosicion().x, 10);
+		ETSIDI::printxy("PULSE LA TECLA -C- PARA CONTINUAR", -5 + mundo.getHombrePosicion().x, 9);
 
-		//Almacenamiento de las monedas adquiridas en el ficehro .txt
+		//Almacenamiento de las monedas adquiridas en el fichero .txt
 		if (flag) {
 			ofstream dato("monedas.txt");
 			int inicial = atoi(m);//Convierte const char* a int para realizar la suma
@@ -135,10 +213,12 @@ void CoordinadorJuego::dibuja()
 			}
 			else cout << "Unable to open file";
 			flag = false;
+
 		}
 		//Vuelve a establecer el flag a true para que
 		//cuando se vuelva a la pantalla de inicio aparezca las monedas totales
 		flagLecturaMonedas = true;
+
 	}
 
 }
