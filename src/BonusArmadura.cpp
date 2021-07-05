cccc3e38cd71dfe24ea8a2e7a12f2924880f1535
Marth98 <47816517+martinrteran@users.noreply.github.com>
2021-07-06 01:33:53 +0200

    Update BonusArmadura.cpp

--- src/BonusArmadura.cpp
+++ src/BonusArmadura.cpp
@@ -5,18 +5,21 @@
 BonusArmadura:: BonusArmadura() :bonus(), S_arm("imagenes/Armadura.png")
 {
 	S_arm.setCenter(posicion.x,posicion.y);
+	S_arm.setPos(posicion.x, posicion.y);
 	S_arm.setSize(1.0f,1.0f);
 }
 BonusArmadura::BonusArmadura(float x, float y):bonus(x,y),S_arm("imagenes/Armadura.png")
 {	// En el constructor de bonus se inicializa la posicion
-	S_arm.setCenter(posicion.x, posicion.y);
-	S_arm.setSize(1.0f, 1.0f);
+	S_arm.setCenter(posicion.x,posicion.y);
+	S_arm.setPos(posicion.x, posicion.y);
+	S_arm.setSize(1.0f,1.0f);
 }
 
 BonusArmadura::BonusArmadura(vector2D A) :bonus(A), S_arm("imagenes/Armadura.png")
 {	// En el constructor de bonus se inicializa la posicion
-	S_arm.setCenter(posicion.x, posicion.y);
-	S_arm.setSize(1.0f, 1.0f);
+	S_arm.setCenter(posicion.x,posicion.y);
+	S_arm.setPos(posicion.x, posicion.y);
+	S_arm.setSize(1.0f,1.0f);
 }
 BonusArmadura::~BonusArmadura()
 {}
