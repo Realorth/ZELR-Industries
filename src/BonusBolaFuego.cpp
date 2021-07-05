c0411d51a135de980a069f934a4eea762e551e23
Marth98 <47816517+martinrteran@users.noreply.github.com>
2021-07-06 01:34:29 +0200

    Update BonusBolaFuego.cpp

--- src/BonusBolaFuego.cpp
+++ src/BonusBolaFuego.cpp
@@ -4,6 +4,7 @@
 bonusBolaFuego::bonusBolaFuego() :S_BF("imagenes/bolaFuego.png"),bonus()
 {
 	S_BF.setCenter(posicion.x, posicion.y);
+	S_BF.setPos(posicion.x, posicion.y);
 	S_BF.setSize(1.0f, 1.0f);
 }
 bonusBolaFuego::~bonusBolaFuego()
@@ -20,14 +21,14 @@ void bonusBolaFuego::dibuja() {
 
 bonusBolaFuego::bonusBolaFuego(float x, float y) :S_BF("imagenes/bolaFuego.png"), bonus(x,y)
 {
-
 	S_BF.setCenter(posicion.x, posicion.y);
+	S_BF.setPos(posicion.x, posicion.y);
 	S_BF.setSize(1.0f, 1.0f);
 }
 
 bonusBolaFuego::bonusBolaFuego(vector2D a) :S_BF("imagenes/bolaFuego.png"), bonus(a)
-{
-	S_BF.setCenter(posicion.x, posicion.y);
+{	S_BF.setCenter(posicion.x, posicion.y);
+	S_BF.setPos(posicion.x, posicion.y);
 	S_BF.setSize(1.0f, 1.0f);
 }
 
