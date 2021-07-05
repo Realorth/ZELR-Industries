f0ca00b935609bc56a93e2f38d61101b5424af02
Aiwen <47836762+azhou12@users.noreply.github.com>
2021-07-06 00:24:25 +0200

    Update arquero.h

--- src/arquero.h
+++ src/arquero.h
@@ -4,7 +4,7 @@
 class arquero : public hombre
 {
 protected:
-	SpriteSequence spritearquero{ "imagenes/arquero.png", 3 };
+	SpriteSequence spritearquero{ "imagenes/arquero1.png", 3 };
 
 public:
 	void dibuja();
