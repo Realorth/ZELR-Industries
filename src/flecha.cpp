8d1a4341a8912b2e79393ab8f33d66a4ebe63d9b
Aiwen <47836762+azhou12@users.noreply.github.com>
2021-07-06 17:10:56 +0200

    Update flecha.cpp

--- src/flecha.cpp
+++ src/flecha.cpp
@@ -1,11 +1,20 @@
 #include "flecha.h"
 
-flecha::flecha() :disparo("imagenes/flecha.png")
+flecha::flecha() :disparo(),S_Flecha("imagenes/flecha.png")
 {
-
+	aceleracion.x = 9.8;
+	aceleracion.y = 0;
+	S_Flecha.setCenter(0.3f, 0.5f);
+	S_Flecha.setSize(1.5f, 1.5f);
 
 }
 
 flecha::~flecha()
 {}
 
+void flecha::dibuja() {
+	glPushMatrix();
+	glTranslatef(posicion.x, posicion.y, 0);
+	S_Flecha.draw();
+	glPopMatrix();
+}
