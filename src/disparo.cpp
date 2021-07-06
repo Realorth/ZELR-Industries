1d719771c1f75755dfc07066f72789b8459f1087
Aiwen <47836762+azhou12@users.noreply.github.com>
2021-07-06 15:36:39 +0200

    Update disparo.cpp

--- src/disparo.cpp
+++ src/disparo.cpp
@@ -19,20 +19,7 @@ disparo::~disparo()
 
 void disparo::dibuja()
 {
-	// TODO: Agregar aquí el código de implementación.
-	/*glColor3f(0.0f, 1.0f, 1.0f);*/
-	glPushMatrix();
-	glTranslatef(posicion.x, posicion.y, 0);
-	//glutSolidSphere(radio, 20, 20);
-	sprite.draw();
-	glPopMatrix();
-
-	glColor3f(0.0f, 255.0f, 255.0f);
-	glBegin(GL_LINES);
-	glVertex2f(origen.x, origen.y);
-	glVertex2f(posicion.x, posicion.y);
-	glEnd();
-	glFlush();
+	
 }
 
 
