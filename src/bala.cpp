f90cc4b73d4fcb0804fad3291bdb880ea337e1c6
Aiwen <47836762+azhou12@users.noreply.github.com>
2021-07-06 15:35:21 +0200

    Update bala.cpp

--- src/bala.cpp
+++ src/bala.cpp
@@ -1,13 +1,19 @@
-#include "bala.h"
-
-bala::bala() :disparo("imagenes/disparo.png")
+bala::bala() :disparo(),SBala("imagenes/disparo.png")
 {
 	aceleracion.x = 9.8;
 	aceleracion.y = 0;
-	sprite.setCenter(0.3f, 0.5f);
-	sprite.setSize(1.5f, 1.5f);
+	SBala.setCenter(0.3f, 0.5f);
+	SBala.setSize(1.5f, 1.5f);
 
 }
 
 bala::~bala()
 {}
+
+void bala::dibuja()
+{
+	glPushMatrix();
+	glTranslatef(posicion.x, posicion.y, 0);
+	SBala.draw();
+	glPopMatrix();
+}
