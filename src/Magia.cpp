dae6032b23e90ce3b768f4ca6ce6ef3105fdfc24
Aiwen <47836762+azhou12@users.noreply.github.com>
2021-07-07 23:38:36 +0200

    Update Magia.cpp

--- src/Magia.cpp
+++ src/Magia.cpp
@@ -1,43 +1,22 @@
 #include "Magia.h"
 
-Magia::Magia():disparo(), SMagia("imagenes/magia.png")
+Magia::Magia():disparo()
 {
-	aceleracion.x = 9.8;
-	aceleracion.y = 0;
-	SMagia.setCenter(posicion.x, posicion.y);
-	SMagia.setPos(posicion.x, posicion.y);
-	SMagia.setSize(1.5f, 1.5f);
+	textura = "imagenes/magia.png";
 }
 
-Magia::Magia(float x, float y) :disparo(x, y), SMagia("imagenes/magia.png")
+Magia::Magia(float x, float y) :disparo(x, y, "imagenes/magia.png")
 {
-	SMagia.setCenter(posicion.x, posicion.y);
-	SMagia.setPos(posicion.x, posicion.y);
-	SMagia.setSize(1.5f, 1.5f);
 }
 
-Magia::Magia(vector2D a) : disparo(a), SMagia("imagenes/magia.png")
+Magia::Magia(vector2D a) : disparo(a, "imagenes/magia.png")
 {
-	SMagia.setCenter(posicion.x, posicion.y);
-	SMagia.setPos(posicion.x, posicion.y);
-	SMagia.setSize(1.5f, 1.5f);
 }
 
-Magia::Magia(vector2D a, vector2D origen) : disparo(a, origen), SMagia("imagenes/magia.png")
-{
-	SMagia.setCenter(posicion.x, posicion.y);
-	SMagia.setPos(posicion.x, posicion.y);
-	SMagia.setSize(1.5f, 1.5f);
+Magia::Magia(vector2D a, vector2D origen) : disparo(a, origen, "imagenes/magia.png")
+{	
 }
 
 Magia::~Magia()
 {
-}
-
-void Magia::dibuja()
-{
-	glPushMatrix();
-	glTranslatef(posicion.x, posicion.y+0.4, 0);
-	SMagia.draw();
-	glPopMatrix();
 }
