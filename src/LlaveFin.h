ea5fa33adeef2ca6ff03bc820867e2d22457fb43
Aiwen <47836762+azhou12@users.noreply.github.com>
2021-07-07 20:28:19 +0200

    Update LlaveFin.h

--- src/LlaveFin.h
+++ src/LlaveFin.h
@@ -3,12 +3,11 @@
 class LlaveFin :public bonus
 {
 protected:
-	ETSIDI::Sprite S_llave;
+	
 public:
 	LlaveFin();
 	LlaveFin(float, float);
 	LlaveFin(vector2D);
 	virtual ~LlaveFin();
-
-	void dibuja();//Virtual
+	void dibuja();
 };
