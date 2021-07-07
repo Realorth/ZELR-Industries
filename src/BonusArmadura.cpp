64bb3f8a5b2508583acaa5b8401343444701508d
Aiwen <47836762+azhou12@users.noreply.github.com>
2021-07-07 20:11:08 +0200

    Update BonusArmadura.cpp

--- src/BonusArmadura.cpp
+++ src/BonusArmadura.cpp
@@ -2,30 +2,19 @@
 #include "ETSIDI.h"
 #include <iostream>
 
-BonusArmadura:: BonusArmadura() :bonus(), S_arm("imagenes/Armadura.png")
+BonusArmadura:: BonusArmadura() :bonus()
 {
-	S_arm.setCenter(posicion.x,posicion.y);
-	S_arm.setPos(posicion.x, posicion.y);
-	S_arm.setSize(1.0f,1.0f);
+	textura = "imagenes/Armadura.png";
+
 }
-BonusArmadura::BonusArmadura(float x, float y):bonus(x,y),S_arm("imagenes/Armadura.png")
+BonusArmadura::BonusArmadura(float x, float y):bonus(x,y, "imagenes/Armadura.png")
 {	// En el constructor de bonus se inicializa la posicion
-	S_arm.setCenter(posicion.x,posicion.y);
-	S_arm.setPos(posicion.x, posicion.y);
-	S_arm.setSize(1.0f,1.0f);
+	
 }
 
-BonusArmadura::BonusArmadura(vector2D A) :bonus(A), S_arm("imagenes/Armadura.png")
+BonusArmadura::BonusArmadura(vector2D A) :bonus(A, "imagenes/Armadura.png")
 {	// En el constructor de bonus se inicializa la posicion
-	S_arm.setCenter(posicion.x,posicion.y);
-	S_arm.setPos(posicion.x, posicion.y);
-	S_arm.setSize(1.0f,1.0f);
+	
 }
 BonusArmadura::~BonusArmadura()
 {}
-void BonusArmadura::dibuja() {
-	glPushMatrix();
-	glTranslatef(posicion.x, posicion.y, 0);
-	S_arm.draw();
-	glPopMatrix();
-}
