d3b1cee6e7fdaa921cebaa83e99b4dde46a1be09
Aiwen <47836762+azhou12@users.noreply.github.com>
2021-07-07 17:41:07 +0200

    Update Magia.cpp

--- src/Magia.cpp
+++ src/Magia.cpp
@@ -4,7 +4,29 @@ Magia::Magia():disparo(), SMagia("imagenes/magia.png")
 {
 	aceleracion.x = 9.8;
 	aceleracion.y = 0;
-	SMagia.setCenter(0.3f, 0.5f);
+	SMagia.setCenter(posicion.x, posicion.y);
+	SMagia.setPos(posicion.x, posicion.y);
+	SMagia.setSize(1.5f, 1.5f);
+}
+
+Magia::Magia(float x, float y) :disparo(x, y), SMagia("imagenes/magia.png")
+{
+	SMagia.setCenter(posicion.x, posicion.y);
+	SMagia.setPos(posicion.x, posicion.y);
+	SMagia.setSize(1.5f, 1.5f);
+}
+
+Magia::Magia(vector2D a) : disparo(a), SMagia("imagenes/magia.png")
+{
+	SMagia.setCenter(posicion.x, posicion.y);
+	SMagia.setPos(posicion.x, posicion.y);
+	SMagia.setSize(1.5f, 1.5f);
+}
+
+Magia::Magia(vector2D a, vector2D origen) : disparo(a, origen), SMagia("imagenes/magia.png")
+{
+	SMagia.setCenter(posicion.x, posicion.y);
+	SMagia.setPos(posicion.x, posicion.y);
 	SMagia.setSize(1.5f, 1.5f);
 }
 
@@ -15,7 +37,7 @@ Magia::~Magia()
 void Magia::dibuja()
 {
 	glPushMatrix();
-	glTranslatef(posicion.x, posicion.y+1.0, 0);
+	glTranslatef(posicion.x, posicion.y+0.4, 0);
 	SMagia.draw();
 	glPopMatrix();
 }
