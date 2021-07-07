9ef4aa35b4d9f864dabf0e0fdf3489f3e9d4be02
Aiwen <47836762+azhou12@users.noreply.github.com>
2021-07-07 23:34:51 +0200

    Update disparoespecial.cpp

--- src/disparoespecial.cpp
+++ src/disparoespecial.cpp
@@ -2,44 +2,21 @@
 #include "freeglut.h"
 
 
-disparoespecial::disparoespecial():disparo(),AtaqueBFuego("imagenes/AtaqueBolaFuego.png")
+disparoespecial::disparoespecial():disparo()
 {
-	aceleracion.x = 9.8;
-	aceleracion.y = 0;
-	AtaqueBFuego.setCenter(posicion.x, posicion.y);
-	AtaqueBFuego.setPos(posicion.x, posicion.y);
-	AtaqueBFuego.setSize(2.5f, 2.5f);
-	
-
+	textura = "imagenes/AtaqueBolaFuego.png";
 }
-disparoespecial::disparoespecial(float x, float y) :disparo(x,y), AtaqueBFuego("imagenes/AtaqueBolaFuego.png")
+disparoespecial::disparoespecial(float x, float y) :disparo(x,y, "imagenes/AtaqueBolaFuego.png")
 {
-	// En el constructor de bonus se inicializa la posicion
-	AtaqueBFuego.setCenter(posicion.x, posicion.y);
-	AtaqueBFuego.setPos(posicion.x, posicion.y);
-	AtaqueBFuego.setSize(2.5f, 2.5f);
 }
-disparoespecial::disparoespecial(vector2D a) :disparo(a), AtaqueBFuego("imagenes/AtaqueBolaFuego.png")
+disparoespecial::disparoespecial(vector2D a) :disparo(a, "imagenes/AtaqueBolaFuego.png")
 {
-	// En el constructor de bonus se inicializa la posicion
-	AtaqueBFuego.setCenter(posicion.x, posicion.y);
-	AtaqueBFuego.setPos(posicion.x, posicion.y);
-	AtaqueBFuego.setSize(2.5f, 2.5f);
 }
-disparoespecial::disparoespecial(vector2D a, vector2D origen):disparo(a,origen), AtaqueBFuego("imagenes/AtaqueBolaFuego.png")
+disparoespecial::disparoespecial(vector2D a, vector2D origen):disparo(a,origen, "imagenes/AtaqueBolaFuego.png")
 {
-	// En el constructor de bonus se inicializa la posicion
-	AtaqueBFuego.setCenter(posicion.x, posicion.y);
-	AtaqueBFuego.setPos(posicion.x, posicion.y);
-	AtaqueBFuego.setSize(2.5f, 2.5f);
 }
 disparoespecial::~disparoespecial()
 {}
 
-void disparoespecial::dibuja()
-{
-	glPushMatrix();
-	glTranslatef(posicion.x, posicion.y + 0.5, 0);
-	AtaqueBFuego.draw();
-	glPopMatrix();
-}
+
+
