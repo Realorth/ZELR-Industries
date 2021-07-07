728626236055ee055f62eab244d9ea3d48cb2ba7
Aiwen <47836762+azhou12@users.noreply.github.com>
2021-07-07 23:36:52 +0200

    Update flecha.h

--- src/flecha.h
+++ src/flecha.h
@@ -2,16 +2,12 @@
 #include "disparo.h"
 class flecha :public disparo
 {
-protected:
-	ETSIDI::Sprite S_Flecha;
-
 public:
 	flecha();
 	flecha(float, float);
 	flecha(vector2D);
 	flecha(vector2D, vector2D);
 	virtual ~flecha();
-	void dibuja();
 };
 
 
