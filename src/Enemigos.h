74b186f539b51195f9082801876bb510221fea1f
zhanyaolin <47865716+zhanyaolin@users.noreply.github.com>
2021-07-07 21:16:08 +0200

    Update Enemigos.h

--- src/Enemigos.h
+++ src/Enemigos.h
@@ -8,40 +8,44 @@ protected:
 	ETSIDI::SpriteSequence sprite_enemigo;
 	int tamano_secuencia = 0;
 	vector2D tamano;
-	//------------------------------------------------------------------------------------------------------------------------------------------------------
-	int rango;				//distancia a partir de la cual reaccionan al hombre
-	int vida = 1, ataque = 1, armadura = 0;
+
+
+	float rango;	//distancia a partir de la cual 
+	// reaccionan ante una entidad que atacaria
+	int vida = 1;
+	int ataque = 1;
+	int armadura = 0;
 	float velataque = 0;
-	//------------------------------------------------------------------------------------------------------------------------------------------------------
 
 public:
 	Enemigos();
 	Enemigos(float x, float y,const char*,int tamano_secuencia);
 	Enemigos(vector2D a, std::string, int tamano_secuencia);
 	Enemigos(vector2D a, const char* b, int tamano_secuencia);
+
+	Enemigos(vector2D pos, vector2D tam, float r, int hp, int armor, int atk, float vatk, const char* b, int n_secuencias);
 	
-	Enemigos(vector2D pos, vector2D tam, int r, int hp, int armor, int atk, float vatk, const char* b, int n_secuencias);		//---------------
-
-
 	virtual ~Enemigos();
 	virtual void dibuja();
-	//------------------------------------------------------------------------------------------------------------------------------------------------------
-	//virtual bool ataca(); 
-	virtual void ataca();
-	int GetRango() { return rango; }																 		
+	virtual void ataca(); 
+
+	int GetRango() { return rango; }
 	void SetRango(int r) { rango = r; }
 	virtual void reacciona() {}
+
 	void SetVida(int hp) { vida = hp; }
-	void SetAtaque(int atk) { ataque = atk; }
-	void GetVAtaque(float vatk) { velataque = vatk; }
-	void SetArmadura(int a) { armadura = a; }
 	int Getvida() { return vida; }
-	int GetAtaque() { return ataque; }
+
+	void SetArmadura(int a) { armadura = a; }
 	int GetArmadura() { return armadura; }
+
+	int GetAtaque() { return ataque; }
+	void SetAtaque(int atk) { ataque = atk; }
+
 	vector2D GetTam() { return tamano; }
 	void SetTam(vector2D tam) { tamano = tam; }
-	void SetTam(float ancho, float alto) { tamano.x = ancho; tamano.y = alto; }
-	//------------------------------------------------------------------------------------------------------------------------------------------------------
+	void SetTam(float ancho, float alto) { tamano.set(ancho,alto); }
 	
+	void gira() { velocidad.x = -velocidad.x; }
 };
 
