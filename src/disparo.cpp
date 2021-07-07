78be0d69722355f69464c39122afa890697d8bfe
Aiwen <47836762+azhou12@users.noreply.github.com>
2021-07-07 23:33:04 +0200

    Update disparo.cpp

--- src/disparo.cpp
+++ src/disparo.cpp
@@ -2,13 +2,39 @@
 #include "freeglut.h"
 #include <math.h>
 #include "hombre.h"
+#include <string>
 
-disparo::disparo() :objetomovil()
+disparo::disparo() :objetomovil(), textura("imagenes/disparo.png")
 {
 	aceleracion.x = 9.8;
 	aceleracion.y = 0;
-	
+	textura.setSize(1.5f, 1.5f);
+	textura.setPos(posicion.x, posicion.y);
+	textura.setCenter(posicion.x, posicion.y);
+}
 
+disparo::disparo(float x, float y, const char* b) : objetomovil(x, y), textura(b)
+{
+	if (!strcmp(b, "imagenes/AtaqueBolaFuego.png")) textura.setSize(2.5f, 2.5f);//Mas grande
+	else textura.setSize(1.5f, 1.5f);
+	textura.setPos(posicion.x, posicion.y);
+	textura.setCenter(posicion.x, posicion.y);
+}
+
+disparo::disparo(vector2D a, const char* b) : objetomovil(a), textura(b)
+{
+	if (!strcmp(b, "imagenes/AtaqueBolaFuego.png")) textura.setSize(2.5f, 2.5f);//Mas grande
+	else textura.setSize(1.5f, 1.5f);
+	textura.setPos(posicion.x, posicion.y);
+	textura.setCenter(posicion.x, posicion.y);
+}
+
+disparo::disparo(vector2D a, vector2D origen, const char* b) : objetomovil(a), origen(origen),textura(b)
+{
+	if (!strcmp(b, "imagenes/AtaqueBolaFuego.png")) textura.setSize(2.5f, 2.5f);//Mas grande
+	else textura.setSize(1.5f, 1.5f);
+	textura.setPos(posicion.x, posicion.y);
+	textura.setCenter(posicion.x, posicion.y);
 }
 
 
@@ -18,7 +44,10 @@ disparo::~disparo()
 
 void disparo::dibuja()
 {
-	
+	glPushMatrix();
+	glTranslatef(posicion.x, posicion.y+0.3, 0);
+	textura.draw();
+	glPopMatrix();
 }
 
 
@@ -45,3 +74,6 @@ float disparo::GetLongitud()
 
 	return sqrt(distancia.x*distancia.x+distancia.y*distancia.y);
 }
+
+
+
