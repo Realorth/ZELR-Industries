6b9b369b642b5161a22b6258f83aa0eb3a7fe27d
Aiwen <47836762+azhou12@users.noreply.github.com>
2021-07-07 23:36:22 +0200

    Update bala.cpp

--- src/bala.cpp
+++ src/bala.cpp
@@ -1,43 +1,20 @@
 #include "bala.h"
-bala::bala() :disparo(),SBala("imagenes/disparo.png")
+bala::bala() :disparo()
 {
-	aceleracion.x = 9.8;
-	aceleracion.y = 0;
-	SBala.setCenter(posicion.x, posicion.y);
-	SBala.setPos(posicion.x, posicion.y);
-	SBala.setSize(1.5f, 1.5f);
-
+	textura = "imagenes/disparo.png";
 }
 
-bala::bala(float x, float y) :disparo(x, y), SBala("imagenes/disparo.png")
+bala::bala(float x, float y) :disparo(x, y, "imagenes/disparo.png")
 {
-	SBala.setCenter(posicion.x, posicion.y);
-	SBala.setPos(posicion.x, posicion.y);
-	SBala.setSize(1.5f, 1.5f);
 }
 
-bala::bala(vector2D a) : disparo(a), SBala("imagenes/disparo.png")
+bala::bala(vector2D a) : disparo(a, "imagenes/disparo.png")
 {
-	SBala.setCenter(posicion.x, posicion.y);
-	SBala.setPos(posicion.x, posicion.y);
-	SBala.setSize(1.5f, 1.5f);
 }
 
-bala::bala(vector2D a, vector2D origen) : disparo(a, origen), SBala("imagenes/disparo.png")
+bala::bala(vector2D a, vector2D origen) : disparo(a, origen, "imagenes/disparo.png")
 {
-	SBala.setCenter(posicion.x, posicion.y);
-	SBala.setPos(posicion.x, posicion.y);
-	SBala.setSize(1.5f, 1.5f);
 }
 
 bala::~bala()
 {}
-
-void bala::dibuja()
-{
-	glPushMatrix();
-	glTranslatef(posicion.x, posicion.y+0.3, 0);
-	SBala.draw();
-	glPopMatrix();
-}
-
