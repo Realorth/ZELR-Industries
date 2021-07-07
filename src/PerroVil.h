f6e0bfe0efef695262c3af9ac5591ae021b0a5c4
Realorth <karmyneitor@gmail.com>
2021-07-07 13:49:30 +0200

    Update PerroVil.h

--- src/PerroVil.h
+++ src/PerroVil.h
@@ -1,15 +1,18 @@
 #pragma once
 #include "Enemigos.h"
-class PerroVil :
-    public Enemigos
+class PerroVil :public Enemigos
 {
 public:
-    PerroVil(vector2D a) :Enemigos(a, "imagenes/wolfy.png", 4) {
+    //PerroVil(vector2D a) :Enemigos(a, "imagenes/wolfy.png", 4) {}
+    PerroVil():Enemigos(0, 0, "imagenes/wolfy.png", 4) { }
     
-    }
-    PerroVil():Enemigos(0, 0, "imagenes/wolfy.png", 4) {
-  
-    }
-    
+    PerroVil(vector2D pos, vector2D tam, int r, int hp, int armor, int atk, float vatk)
+        :Enemigos(pos, tam, r, hp, armor, atk, vatk, "imagenes/wolfy.png", 4) {}
+    PerroVil(vector2D pos):Enemigos(pos, (2.5, 2.5), 1, 2, 0, 1, 0, "imagenes/wolfy.png", 4) {}      //valores por defecto de perro cualquiera
+
+
+    void ataca();
+    void gira();
+    void reacciona();
 };
 
