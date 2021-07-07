6f09ed3888faf3fc3fe9b17b821eb1c2962d96f3
zhanyaolin <47865716+zhanyaolin@users.noreply.github.com>
2021-07-08 01:20:00 +0200

    Update AcolitoOscuro.cpp

--- src/AcolitoOscuro.cpp
+++ src/AcolitoOscuro.cpp
@@ -1,16 +1,30 @@
 #include "AcolitoOscuro.h"
 
-
-AcolitoOscuro::AcolitoOscuro(): Enemigos(0, 0, "imagenes/brujo3.png", 4)
+void AcolitoOscuro::gira()
 {
-	vida = 2;
-	ataque = 2;
-	aceleracion=velocidad = (0,0);
+	vector2D aux = GetVel();
+	SetVel(0, 0);
+	if (aux.x > 0)			SetPos(GetPos().x - 0.5f, 0);
+	else if (aux.x < 0)		SetPos(GetPos().x + 0.5f, 0);
+	SetVel(-aux.x, 0);
 }
-AcolitoOscuro::~AcolitoOscuro(){}
-
-void AcolitoOscuro::ataca() {
-	//dispara
+AcolitoOscuro::~AcolitoOscuro()
+{
+}
+AcolitoOscuro::AcolitoOscuro(float x, float y) :
+	Enemigos(x, y, "imagenes/Brujo2.png", 4)
+{
+	velocidad.x = 3.0f;
 }
 
+AcolitoOscuro::AcolitoOscuro(vector2D a) :
+	Enemigos(a, "imagenes/Brujo2.png", 4)
+{
+	velocidad.x = 3.0f;
+}
 
+AcolitoOscuro::AcolitoOscuro() :
+	Enemigos(0, 0, "imagenes/Brujo2.png", 4)
+{
+	velocidad.x = 3.0f;
+}
