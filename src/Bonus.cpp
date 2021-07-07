c6ca2e91487cebddd437904dc1d4935e23d398cf
Aiwen <47836762+azhou12@users.noreply.github.com>
2021-07-07 20:17:15 +0200

    Update Bonus.cpp

--- src/Bonus.cpp
+++ src/Bonus.cpp
@@ -3,11 +3,32 @@
 #include "freeglut.h"
 #include "ETSIDI.h"
 
-bonus::bonus():objetomovil()
+bonus::bonus():objetomovil(),textura("imagenes/coin.png")
 {
+	textura.setSize(1.0f, 1.0f);
+	textura.setPos(posicion.x, posicion.y);
+	textura.setCenter(0.5f, 0.5f);
+}
+
+bonus::bonus(float x, float y, const char* b) : objetomovil(x, y), textura(b)
+{
+	textura.setSize(1.0f, 1.0f);
+	textura.setPos(posicion.x, posicion.y);
+	textura.setCenter(0.5f, 0.5f);
+}
+
+bonus::bonus(vector2D a, const char* b) :objetomovil(a), textura(b)
+{
+	textura.setSize(1.0f, 1.0f);
+	textura.setPos(posicion.x, posicion.y);
+	textura.setCenter(0.5f, 0.5f);
 }
 
 bonus::~bonus()
 {}
 
+void bonus::dibuja()
+{
+	textura.draw();
+}
 
