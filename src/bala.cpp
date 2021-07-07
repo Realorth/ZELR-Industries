ed7ac373361dfaf83757c8f478330e31f96d05be
Aiwen <47836762+azhou12@users.noreply.github.com>
2021-07-07 17:38:41 +0200

    Update bala.cpp

--- src/bala.cpp
+++ src/bala.cpp
@@ -3,9 +3,31 @@ bala::bala() :disparo(),SBala("imagenes/disparo.png")
 {
 	aceleracion.x = 9.8;
 	aceleracion.y = 0;
-	SBala.setCenter(0.3f, 0.5f);
+	SBala.setCenter(posicion.x, posicion.y);
+	SBala.setPos(posicion.x, posicion.y);
 	SBala.setSize(1.5f, 1.5f);
 
+}
+
+bala::bala(float x, float y) :disparo(x, y), SBala("imagenes/disparo.png")
+{
+	SBala.setCenter(posicion.x, posicion.y);
+	SBala.setPos(posicion.x, posicion.y);
+	SBala.setSize(1.5f, 1.5f);
+}
+
+bala::bala(vector2D a) : disparo(a), SBala("imagenes/disparo.png")
+{
+	SBala.setCenter(posicion.x, posicion.y);
+	SBala.setPos(posicion.x, posicion.y);
+	SBala.setSize(1.5f, 1.5f);
+}
+
+bala::bala(vector2D a, vector2D origen) : disparo(a, origen), SBala("imagenes/disparo.png")
+{
+	SBala.setCenter(posicion.x, posicion.y);
+	SBala.setPos(posicion.x, posicion.y);
+	SBala.setSize(1.5f, 1.5f);
 }
 
 bala::~bala()
@@ -14,7 +36,8 @@ bala::~bala()
 void bala::dibuja()
 {
 	glPushMatrix();
-	glTranslatef(posicion.x, posicion.y+1, 0);
+	glTranslatef(posicion.x, posicion.y+0.3, 0);
 	SBala.draw();
 	glPopMatrix();
 }
+
