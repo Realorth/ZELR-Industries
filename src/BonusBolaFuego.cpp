5aa3f76565c2dabb93c5df57fa0b81ebdc468857
Aiwen <47836762+azhou12@users.noreply.github.com>
2021-07-07 20:12:37 +0200

    Update BonusBolaFuego.cpp

--- src/BonusBolaFuego.cpp
+++ src/BonusBolaFuego.cpp
@@ -1,34 +1,20 @@
 #include "bonusBolaFuego.h"
 #include "ETSIDI.h"
 
-bonusBolaFuego::bonusBolaFuego() :S_BF("imagenes/bolaFuego.png"),bonus()
+bonusBolaFuego::bonusBolaFuego() :bonus()
 {
-	S_BF.setCenter(posicion.x, posicion.y);
-	S_BF.setPos(posicion.x, posicion.y);
-	S_BF.setSize(1.0f, 1.0f);
+	textura = "imagenes/bolaFuego.png";
+
 }
 bonusBolaFuego::~bonusBolaFuego()
 {}
-void bonusBolaFuego::dibuja() {
-	glPushMatrix();
-	glTranslatef(posicion.x, posicion.y,0);
-	S_BF.draw();
-	glTranslatef(-posicion.x, -posicion.y, 0);
-	glPopMatrix();
 
 
-}
-
-bonusBolaFuego::bonusBolaFuego(float x, float y) :S_BF("imagenes/bolaFuego.png"), bonus(x,y)
+bonusBolaFuego::bonusBolaFuego(float x, float y) : bonus(x,y, "imagenes/bolaFuego.png")
 {
-	S_BF.setCenter(posicion.x, posicion.y);
-	S_BF.setPos(posicion.x, posicion.y);
-	S_BF.setSize(1.0f, 1.0f);
+	
 }
 
-bonusBolaFuego::bonusBolaFuego(vector2D a) :S_BF("imagenes/bolaFuego.png"), bonus(a)
-{	S_BF.setCenter(posicion.x, posicion.y);
-	S_BF.setPos(posicion.x, posicion.y);
-	S_BF.setSize(1.0f, 1.0f);
+bonusBolaFuego::bonusBolaFuego(vector2D a) : bonus(a, "imagenes/bolaFuego.png")
+{	
 }
-
