5df01da4384eb6f43f13c71615820b3b9679dc89
zhanyaolin <47865716+zhanyaolin@users.noreply.github.com>
2021-07-08 01:19:38 +0200

    Update AcolitoOscuro.h

--- src/AcolitoOscuro.h
+++ src/AcolitoOscuro.h
@@ -3,23 +3,15 @@
 #include "Interaccion.h"
 class AcolitoOscuro :public Enemigos
 {
-	SpriteSequence spriteacolito{ "imagenes/brujo3.png", 4 };
-	int sentido = -1;
 public:
-	
+	~AcolitoOscuro();
+
+	AcolitoOscuro(float x, float y);
+	AcolitoOscuro(vector2D a);
 	AcolitoOscuro();
 
-	AcolitoOscuro(vector2D pos, vector2D tam, int r, int hp, int armor, int atk, float vatk)	                     //---------------
-		:Enemigos(pos, tam, r, hp, armor, atk, vatk, "imagenes/brujo3.png", 4) {}
-	AcolitoOscuro(vector2D pos) :Enemigos(pos, (3, 3), 25, 2, 0, 2, 85, "imagenes/brujo3.png", 4) {}      //valores por defecto de acólito cualquiera
-
-
-
-
-	int GetSentido() { return sentido; }
-	void SetSentido(int s) { sentido = s; }
-	void ataca();
+	void mueve(float t) { objetomovil::mueve(t); if (velocidad.y < -5.0)velocidad.y = -5.0f; }
+	void gira();
 	
-	~AcolitoOscuro();
 };
 
