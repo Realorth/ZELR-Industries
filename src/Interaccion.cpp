35cf985a5bbff6c46075415802e8dc2e6963be3e
Realorth <karmyneitor@gmail.com>
2021-07-07 15:05:19 +0200

    Update Interaccion.cpp
    Comprobar colisión hombre-enemigo (151)

--- src/Interaccion.cpp
+++ src/Interaccion.cpp
@@ -42,18 +42,24 @@ void Interaccion::colision(hombre& h, pared p)
 }
 
 
-bool Interaccion::Borde(Enemigos& e, std::vector<pared*> d)
+bool Interaccion::Borde(Enemigos& e, std::vector<pared*> p)
 {	
 	Enemigos aux(e);
 
 	return false;
 }
-void Interaccion::rebote(Enemigos& e, std::vector<pared*> d)
+void Interaccion::rebote(Enemigos& e, std::vector<pared*> d)		//alternativa
 {
 	for (auto i : d) 
 		colision(e, *i);
 	
 }
+void Interaccion::rebote(std::vector<Enemigos*> e, std::vector<pared*> p)		//usada en mundo
+{
+	for (auto i : e)
+		rebote(*i, p);
+}
+
 bool Interaccion::colision(Enemigos& e, pared p)
 {
 	vector2D dir;
@@ -69,17 +75,21 @@ bool Interaccion::colision(Enemigos& e, pared p)
 	return false;
 
 }
-void Interaccion::rebote(std::vector<Enemigos*> a, std::vector<pared*> d)
-{
-	for (auto i : a)
-		rebote(*i, d);
 
-}
-
-void Interaccion::rebote(hombre& h, std::vector<pared*> d)
+void Interaccion::rebote(hombre& h, std::vector<pared*> p)
 {
-	for (auto i : d) 
+	for (auto i : p) 
 		colision(h, *i);
+	/*if (h.GetPos().x < -3.5)									//del estado de ayer
+		h.SetPos(-3.5, h.GetPos().y);
+	if (h.GetPos().x > 293)
+		h.SetPos(293,h.GetPos().y);
+	//for (auto i : p) {
+	//	if(i!=p.front()&&i!=p.back())
+	//	colision(h, *i);
+	//	else
+	//	colision_border(h,p);
+	//}*/
 }
 
 void Interaccion::colision_border(hombre& h, std::vector<pared*> p){
@@ -123,3 +133,40 @@ bool Interaccion::muertecaida(hombre h)
 	return false;
 }
 
+bool Interaccion::colision(objetomovil& e, pared p)
+{
+	vector2D dir;
+	float dif = p.distancia(e.GetPos(), &dir) - 0.1f;
+	if (dif <= 0.0f)
+	{
+		//vector2D v_inicial = h.velocidad;
+		e.SetVel(e.GetVel().x, 0);
+		e.SetPos(e.GetPos() - dir * dif);
+		e.SetPos(e.GetPos().x, e.GetPos().y += 0.001f);
+		return true;
+	}
+	return false;
+}
+
+bool Interaccion::colision(hombre& h, Enemigos& e)
+{
+	vector2D pos = h.GetPos(); //la posicion de la base del hombre
+	vector2D posEnemigo = e.GetPos();
+	pos.y += h.GetAltura() / 2.0f; //posicion del centro
+	float distancia = (posEnemigo - pos.y).modulo();
+	if (distancia < (e.GetTam()).modulo()/2+1.0f)
+		return true;
+	return false;
+}
+
+bool Interaccion::proximidad(hombre& h, Enemigos& e) {
+	vector2D pos = h.GetPos() - e.GetPos();
+	if (pos.modulo() < e.GetRango()) return true;						//si el hombre está a rango
+	else return false;
+}
+
+bool Interaccion::deteccion(hombre& h, Enemigos& e) {   
+	vector2D pos = h.GetPos() - e.GetPos();
+	if (pos.x>0) return true;									//si el hombre está a la derecha
+	else return false;
+}
