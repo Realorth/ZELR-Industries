f13e4d688401335bbce9f035a5b8033ed04b4462
Aiwen <47836762+azhou12@users.noreply.github.com>
2021-07-07 23:37:32 +0200

    Update flecha.cpp

--- src/flecha.cpp
+++ src/flecha.cpp
@@ -1,41 +1,21 @@
 #include "flecha.h"
 
-flecha::flecha() :disparo(),S_Flecha("imagenes/flecha.png")
+flecha::flecha() :disparo()
 {
-	aceleracion.x = 9.8;
-	aceleracion.y = 0;
-	S_Flecha.setCenter(posicion.x, posicion.y);
-	S_Flecha.setPos(posicion.x, posicion.y);
-	S_Flecha.setSize(2.0f, 2.0f);
+	textura = "imagenes/flecha.png";
 }
 
-flecha::flecha(float x, float y) :disparo(x, y), S_Flecha("imagenes/flecha.png")
+flecha::flecha(float x, float y) :disparo(x, y, "imagenes/flecha.png")
 {
-	S_Flecha.setCenter(posicion.x, posicion.y);
-	S_Flecha.setPos(posicion.x, posicion.y);
-	S_Flecha.setSize(2.0f, 2.0f);
 }
 
-flecha::flecha(vector2D a) : disparo(a), S_Flecha("imagenes/flecha.png")
+flecha::flecha(vector2D a) : disparo(a, "imagenes/flecha.png")
 {
-	S_Flecha.setCenter(posicion.x, posicion.y);
-	S_Flecha.setPos(posicion.x, posicion.y);
-	S_Flecha.setSize(2.0f, 2.0f);
 }
 
-flecha::flecha(vector2D a, vector2D origen):disparo(a,origen), S_Flecha("imagenes/flecha.png")
+flecha::flecha(vector2D a, vector2D origen):disparo(a,origen, "imagenes/flecha.png")
 {
-	S_Flecha.setCenter(posicion.x, posicion.y);
-	S_Flecha.setPos(posicion.x, posicion.y);
-	S_Flecha.setSize(2.0f, 2.0f);
 }
 
 flecha::~flecha()
 {}
-
-void flecha::dibuja() {
-	glPushMatrix();
-	glTranslatef(posicion.x, posicion.y+0.4, 0);
-	S_Flecha.draw();
-	glPopMatrix();
-}
