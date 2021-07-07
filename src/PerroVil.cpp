56104e2a93b19e300a698e5b5a51d420d271934e
Aiwen <47836762+azhou12@users.noreply.github.com>
2021-07-08 01:18:18 +0200

    Update PerroVil.cpp

--- src/PerroVil.cpp
+++ src/PerroVil.cpp
@@ -1,14 +1,5 @@
 #include "PerroVil.h"
 
-void PerroVil::gira()
-{
-	vector2D aux = GetVel();
-	SetVel(0, 0);
-	if (aux.x > 0)			SetPos(GetPos().x - 0.5f, 0);
-	else if (aux.x < 0)		SetPos(GetPos().x + 0.5f, 0);
-	SetVel(-aux.x, 0);
-}
-
 PerroVil::PerroVil(float x, float y) :
 	Enemigos(x, y, "imagenes/wolfy.png",4)
 {
@@ -28,7 +19,3 @@ PerroVil::PerroVil():
 }
 
 void PerroVil::ataca() {}
-void PerroVil::reacciona()
-{
-	aceleracion.x = 0.002 + aceleracion.x * 1.2;			//Empieza a patrullar cada vez más rápido
-}
