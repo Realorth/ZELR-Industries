e70b3e8e948c349c488bd65960c9ba2988bff31c
Realorth <karmyneitor@gmail.com>
2021-07-07 13:33:57 +0200

    Update Enemigos.h

--- src/Enemigos.h
+++ src/Enemigos.h
@@ -8,16 +8,36 @@ protected:
 	ETSIDI::SpriteSequence sprite_enemigo;
 	int tamano_secuencia = 0;
 	vector2D tamano;
+	//------------------------------------------------------------------------------------------------------------------------------------------------------
+	int rango;				//distancia a partir de la cual reaccionan al hombre
+	int vida = 1, ataque = 1, armadura = 0;
+	float velataque = 0;
+	//------------------------------------------------------------------------------------------------------------------------------------------------------
+
 public:
 	Enemigos();
 	Enemigos(float x, float y,const char*,int tamano_secuencia);
 	Enemigos(vector2D a, std::string, int tamano_secuencia);
 	Enemigos(vector2D a, const char* b, int tamano_secuencia);
+	
+	Enemigos(vector2D pos, vector2D tam, int r, int hp, int armor, int atk, float vatk, const char* b, int n_secuencias);		//---------------
+
 
 	virtual ~Enemigos();
 	virtual void dibuja();
-	virtual bool ataca(); 
-
+	//------------------------------------------------------------------------------------------------------------------------------------------------------
+	//virtual bool ataca(); 
+	virtual void ataca();
+	int GetRango() { return rango; }																 		
+	void SetRango(int r) { rango = r; }
+	virtual void reacciona() {}
+	void SetVida(int hp) { vida = hp; }
+	void SetAtaque(int atk) { ataque = atk; }
+	void SetArmadura(int a) { armadura = a; }
+	int Getvida() { return vida; }
+	int GetAtaque() { return ataque; }
+	int GetArmadura() { return armadura; }
+	//------------------------------------------------------------------------------------------------------------------------------------------------------
 	
 };
 
