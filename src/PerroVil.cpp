bd08dd6a147566c9050403b4b4184870419abc90
Realorth <karmyneitor@gmail.com>
2021-07-07 13:52:01 +0200

    Update PerroVil.cpp
    giro se deberá llamar al perder la colisión con pared

--- src/PerroVil.cpp
+++ src/PerroVil.cpp
@@ -1,1 +1,16 @@
 #include "PerroVil.h"
+
+void PerroVil::gira()
+{
+	vector2D aux = GetVel();
+	SetVel(0, 0);
+	if (aux.x > 0)			SetPos(GetPos().x - 0.5f, 0);
+	else if (aux.x < 0)		SetPos(GetPos().x + 0.5f, 0);
+	SetVel(-aux.x, 0);
+}
+
+void PerroVil::ataca() {}
+void PerroVil::reacciona() 
+{
+	aceleracion.x = 0.002 + aceleracion.x * 1.2;			//Empieza a patrullar cada vez más rápido
+}
