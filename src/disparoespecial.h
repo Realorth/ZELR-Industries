80923b102bf4ed0ffcb9db9fbee091065609cfd2
Aiwen <47836762+azhou12@users.noreply.github.com>
2021-07-07 23:33:52 +0200

    Update disparoespecial.h

--- src/disparoespecial.h
+++ src/disparoespecial.h
@@ -6,14 +6,10 @@
 
 class disparoespecial : public disparo
 {
-protected:
-	ETSIDI::Sprite AtaqueBFuego;
 public:
 	disparoespecial();
 	disparoespecial(float, float);
 	disparoespecial(vector2D);
 	disparoespecial(vector2D,vector2D);
 	~disparoespecial();
-	void dibuja();
-	
 };
