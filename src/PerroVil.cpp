fae139d338032e22ebd782225cd87b73b37d2a53
Marth98 <47816517+martinrteran@users.noreply.github.com>
2021-07-08 04:12:54 +0200

    Add files via upload

--- src/PerroVil.cpp
+++ src/PerroVil.cpp
@@ -1,21 +1,37 @@
 #include "PerroVil.h"
 
+void PerroVil::gira()
+{
+	vector2D aux = GetVel();
+	SetVel(0, 0);
+	if (aux.x > 0)			SetPos(GetPos().x - 0.5f, 0);
+	else if (aux.x < 0)		SetPos(GetPos().x + 0.5f, 0);
+	SetVel(-aux.x, 0);
+}
+
 PerroVil::PerroVil(float x, float y) :
 	Enemigos(x, y, "imagenes/wolfy.png",4)
 {
 	velocidad.x = 3.0f;
+	vida = 2;
 }
 
 PerroVil::PerroVil(vector2D a):
 	Enemigos(a,"imagenes/wolfy.png",4)
 {
-	velocidad.x =3.0f;
+	velocidad.x = 3.0f;
+	vida = 2;
 }
 
 PerroVil::PerroVil():
 	Enemigos(0,0, "imagenes/wolfy.png", 4)
 {
 	velocidad.x = 3.0f;
+	vida = 2;
 }
 
 void PerroVil::ataca() {}
+void PerroVil::reacciona()
+{
+	aceleracion.x = 0.002 + aceleracion.x * 1.2;			//Empieza a patrullar cada vez más rápido
+}
