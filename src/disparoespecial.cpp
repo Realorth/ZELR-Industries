d9550de79ef12cde0197e3271d32f32e425c4951
Marth98 <47816517+martinrteran@users.noreply.github.com>
2021-07-08 04:26:03 +0200

    Update disparoespecial.cpp

--- src/disparoespecial.cpp
+++ src/disparoespecial.cpp
@@ -1,19 +1,22 @@
 #include "disparoespecial.h"
 #include "freeglut.h"
 
-
 disparoespecial::disparoespecial():disparo()
 {
 	textura = "imagenes/AtaqueBolaFuego.png";
+	SetNombre("Especial");
 }
 disparoespecial::disparoespecial(float x, float y) :disparo(x,y, "imagenes/AtaqueBolaFuego.png")
 {
+	SetNombre("Especial");
 }
 disparoespecial::disparoespecial(vector2D a) :disparo(a, "imagenes/AtaqueBolaFuego.png")
 {
+	SetNombre("Especial");
 }
 disparoespecial::disparoespecial(vector2D a, vector2D origen):disparo(a,origen, "imagenes/AtaqueBolaFuego.png")
 {
+	SetNombre("Especial");
 }
 disparoespecial::~disparoespecial()
 {}
