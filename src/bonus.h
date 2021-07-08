eee51c6257e45d37ce5ec8814537ccacb8b0eee0
Aiwen <47836762+azhou12@users.noreply.github.com>
2021-07-08 16:53:44 +0200

    Update bonus.h

--- src/bonus.h
+++ src/bonus.h
@@ -9,17 +9,15 @@ class bonus : public objetomovil
 {
 protected:
 	string nombreBonus="Moneda";//por defecto se llama moneda
+	ETSIDI::Sprite textura;//Textura que cambia según el tipo de bonus
 public:
 	bonus();//de serie carga la moneda
 	bonus(float x, float y, const char* b);
 	bonus(vector2D a, const char* b);
 	virtual ~bonus();
-
+	string GetNombre() { return nombreBonus; }
+	void SetNombre(string a) { nombreBonus = a; }
 	// funcion principal de dibujo del bonus
 	virtual void dibuja();
 	void SetTextura(const char* b) { textura = b ; }
-	
-protected:
-	ETSIDI::Sprite textura;//Textura que cambia según el tipo de bonus
-	
 };
