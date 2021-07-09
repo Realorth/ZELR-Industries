f90ad84aa4b716a9af28f7888576ae18dcc0e674
Realorth <karmyneitor@gmail.com>
2021-07-09 21:13:53 +0200

    Update Murcielago.cpp

--- src/Murcielago.cpp
+++ src/Murcielago.cpp
@@ -4,16 +4,16 @@
 
 Murcielago::Murcielago() :Enemigos(0, 0, "imagenes/Eagle.png", 4)
 {
-	velocidad.x = 3.6f;
+	velocidad.x = 4.6f;
 	aceleracion = (0, 0);
 }
 Murcielago::Murcielago(float x, float y):Enemigos(x, y, "imagenes/Eagle.png", 4)
 {
-	velocidad.x = 3.6f;
+	velocidad.x = 4.6f;
 	aceleracion = (0, 0);
 }
 Murcielago::Murcielago(vector2D a): Enemigos(a, "imagenes/Eagle.png", 4)
 {
-	velocidad.x = 3.6f;
+	velocidad.x = 4.6f;
 	aceleracion = (0, 0);
 }
