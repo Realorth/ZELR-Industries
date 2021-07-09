3b2ee1f34e4e3028930759319c024f5a849ebd7d
Realorth <karmyneitor@gmail.com>
2021-07-09 20:22:32 +0200

    Update Murcielago.cpp
    Eliminado el efecto de la gravedad, ahora no se caerán a las plataformas

--- src/Murcielago.cpp
+++ src/Murcielago.cpp
@@ -4,13 +4,16 @@
 
 Murcielago::Murcielago() :Enemigos(0, 0, "imagenes/Eagle.png", 4)
 {
-	velocidad.x = 3.0f;
+	velocidad.x = 3.6f;
+	aceleracion = (0, 0);
 }
 Murcielago::Murcielago(float x, float y):Enemigos(x, y, "imagenes/Eagle.png", 4)
 {
-	velocidad.x = 3.0f;
+	velocidad.x = 3.6f;
+	aceleracion = (0, 0);
 }
 Murcielago::Murcielago(vector2D a): Enemigos(a, "imagenes/Eagle.png", 4)
 {
-	velocidad.x = 3.0f;
+	velocidad.x = 3.6f;
+	aceleracion = (0, 0);
 }
